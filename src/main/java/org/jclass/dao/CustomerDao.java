193b6ba6a2cd12dca8fb03cebd6d8483d02e2b0b
cwjokaka <1250217678@qq.com>
2017-05-17 01:34:24 +0800

    dd

---
+++ src/main/java/org/jclass/dao/CustomerDao.java
@@ -0,0 +1,11 @@
+package org.jclass.dao;
+
+import org.jclass.jdbc.LsBaseJDBC;
+import org.jclass.model.Customer;
+
+/**
+ * Created by hasee on 2017/5/16.
+ */
+public class CustomerDao extends LsBaseJDBC<Customer>{
+
+}
--- src/test/java/org/jclass/JDBCTest.java
+++ src/test/java/org/jclass/JDBCTest.java
@@ -1,20 +1,26 @@
 package org.jclass;
 
+import org.jclass.dao.CustomerDao;
+import org.jclass.model.Customer;
 import org.junit.Before;
 import org.junit.Test;
+
 
 /**
  * Created by hasee on 2017/5/15.
  */
 public class JDBCTest {
 
+    private CustomerDao customerDao;
+
     @Before
     public void init(){
-
+        customerDao = new CustomerDao();
     }
 
     @Test
-    public void deleteEntityTest(){
+    public void queryEntityTest(){
+        Customer customer = customerDao.queryEntity("SELECT * FROM customer");
 
     }
 }
